bbc99b587770c6bbe292334a9526ad93ee96f358
renz-garcia <renzgarcia.dev@gmail.com>
2025-11-05 03:50:31 +0800

    Users app views.py and urls.py initialized

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def register_view(request):
+    return HttpResponse("User Registration Page")
+
+def login_view(request):
+    return HttpResponse("User Login Page")
+
+def logout_view(request):
+    return HttpResponse("User Logout Page")
+
+def profile_view(request):
+    return HttpResponse("User Profile Page")
