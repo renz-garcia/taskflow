de274605e8f0ea7453fe467a4e922d81657bc447
renz-garcia <renzgarcia.dev@gmail.com>
2025-11-05 08:52:38 +0800

    Setup base.html and splash.html templates

--- taskflow/urls.py
+++ taskflow/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
+app_name = 'taskflow'
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
 ]
