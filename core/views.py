256154e8f268bba95d06e0db3db9c33397fa0037
renz-garcia <renzgarcia.dev@gmail.com>
2025-11-18 16:32:43 +0800

    add base.html and splash.html file

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+def splash(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/splash.html")
